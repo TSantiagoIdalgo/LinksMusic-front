68083226e6b0d0e226549e3cf80db8bcb9caed6e
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-02-21 22:42:20 -0300

    LinksMusic

--- src/utils/hooks/music/useDownload.ts
+++ src/utils/hooks/music/useDownload.ts
@@ -23,7 +23,7 @@ export function useDownload() {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', `${name}.mp3`);
+      link.setAttribute('download', `LinksMusic - ${name}.mp3`);
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
